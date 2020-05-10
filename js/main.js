fbed7f9070c9e9381d88b3c2a601c716eb8445c4
beksultan <turgunbaaev@gmail.com>
2020-05-10 03:04:23 +0300

    change

--- js/main.js
+++ js/main.js
@@ -37,3 +37,102 @@ $(".burger_menu").click(function() {
             $(this).html('<i class="fas fa-bars"></i>');
             }
     });
+
+    // ============== Кнопка "Навверх" ================
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0)
+            $("#toTop").fadeIn();
+        else
+            $("#toTop").fadeOut();
+    });
+    
+        $("#toTop").click(function() {
+            $('body, html').animate({
+                scrollTop:0
+            }, 800);
+        });
+
+    // ============= Функции для меню ==========
+
+    $('#homes').on("click", function(e){
+        e.preventDefault();
+            var top = $(".home").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#abouts').on("click", function(e){
+        e.preventDefault();
+            var top = $(".about_us").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#services').on("click", function(e){
+        e.preventDefault();
+            var top = $(".service_cont").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#portfolios').on("click", function(e){
+        e.preventDefault();
+            var top = $(".portfolio").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#contacts').on("click", function(e){
+        e.preventDefault();
+            var top = $(".footer").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+     // ============= Функции для меню мобильной версий ==========
+
+     $('#home').on("click", function(e){
+        e.preventDefault();
+            var top = $(".home").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#about').on("click", function(e){
+        e.preventDefault();
+            var top = $(".about_us").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#service').on("click", function(e){
+        e.preventDefault();
+            var top = $(".service_cont").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#portfolio').on("click", function(e){
+        e.preventDefault();
+            var top = $(".portfolio").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
+
+    $('#contact').on("click", function(e){
+        e.preventDefault();
+            var top = $("#footer").offset().top;
+        $('html,body').animate({
+           scrollTop: top
+        }, 900);
+    });
